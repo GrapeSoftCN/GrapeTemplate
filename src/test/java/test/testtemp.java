1cc7dacec651995f20931af563e6205b22d63934
GrapeSoftCN <jk282@163.com>
2017-05-08 18:26:07 +0800

    接口修改

---
+++ src/test/java/test/testtemp.java
@@ -0,0 +1,15 @@
+package test;
+
+import httpServer.booter;
+
+public class testtemp {
+	public static void main(String[] args) {
+		booter booter = new booter();
+		 try {
+		 System.out.println("Grapetemp!");
+		 System.setProperty("AppName", "Grapetemp");
+		 booter.start(1005);
+		} catch (Exception e) {
+		}
+	}
+}
